76541c538be07ba17e327fca8f8733889d3ab36b
Amirmohammad_sharbati <amirmohammad.shn84@gmail.com>
2025-05-20 22:59:24 +0330

    Completion of bus with tri-state buffers. May some bugs are still yet

--- bus_with_triStateBuffer/tristate_buf.v
+++ bus_with_triStateBuffer/tristate_buf.v
@@ -1,6 +1,6 @@
 module tristate_buf #(parameter N = 8)( // 8 is default value of N, can be overriden
 	input wire [N-1:0] data_in,	// Data to send to bus
-	input wire oe	// Output enable (1 -> drive bus)
+	input wire oe,	// Output enable (1 -> drive bus)
 	inout wire [N-1:0] bi_data		// Shared bus 
 );
 	
