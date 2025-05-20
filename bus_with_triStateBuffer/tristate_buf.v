a7b0c2cffc2ab41bde0ed004804fc2ca9176f4f3
Amirmohammad_sharbati <amirmohammad.shn84@gmail.com>
2025-05-20 12:31:20 +0330

    Starting practice 2. First step was constructing tri_state buffer.

---
+++ bus_with_triStateBuffer/tristate_buf.v
@@ -0,0 +1,10 @@
+module tristate_buf #(parameter N = 8)( // 8 is default value of N, can be overriden
+	input wire [N-1:0] data_in,	// Data to send to bus
+	input wire oe	// Output enable (1 -> drive bus)
+	inout wire [N-1:0] bi_data		// Shared bus 
+);
+	
+	assign bi_date = oe ? data_in : {N{1'bz}};
+	// If oe is 1, drive the bus with data_in. Else output is Z (high-impedance)
+
+endmodule
